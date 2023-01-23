13712479c604cb25d60c4cedce3e94e08be83e7a
kolosovpetro <kolosovp94@gmail.com>
2023-01-23 23:30:47 +0100

    global suppresions update

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
 [assembly:
+    SuppressMessage("Style", "IDE0072:Add missing cases to switch expression",
+        Justification = "Not applicable for test names", Scope = "module")]
+[assembly:
     SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores",
         Justification = "Not applicable for test names", Scope = "module")]
 [assembly:
