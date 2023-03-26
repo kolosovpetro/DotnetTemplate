ce533d4e6ac7fa96c52d88e1ace30a02ef91bc89
kolosovpetro <kolosovp94@gmail.com>
2023-03-26 13:49:19 +0200

    editor config | git attributes | resharper scan

--- DotnetTemplate/GlobalSuppressions.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly:
-    SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores",
-        Justification = "Not applicable for test names", Scope = "module")]
